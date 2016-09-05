be3418d3024fd35631b94d3381717b21a3297d2d
Philipp_Kozlov <Philipp_Kozlov@EPBYMINW3256>
2016-09-05 13:23:45 +0300

    Added CustomActionInvoker.

--- Day2/ControllersWithCustomFactory/Global.asax.cs
+++ Day2/ControllersWithCustomFactory/Global.asax.cs
@@ -15,6 +15,7 @@ namespace Controllers
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             ControllerBuilder.Current.SetControllerFactory(new CustomControllerFactory());
+            //ControllerBuilder.Current.SetControllerFactory(new DefaultControllerFactory(new CustomControllerActivator()));
         }
     }
 }
